2f4815478bf6362a43909070283f8cecea798e44
Gabe Maentz <gabe@maentz.net>
2021-11-29 17:04:08 -0500

    update prereq code for Terraform Module Objectives

---
+++ terraform/Lab Prerequisites/Terraform Module Sources/server/server.tf
@@ -0,0 +1,28 @@
+variable "ami" {}
+variable "size" {
+  default = "t2.micro"
+}
+variable "subnet_id" {}
+variable "security_groups" {
+  type = list(any)
+}
+resource "aws_instance" "web" {
+  ami                    = var.ami
+  instance_type          = var.size
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_groups
+
+  tags = {
+    "Name"        = "Server from Module"
+    "Environment" = "Training"
+  }
+}
+
+output "public_ip" {
+  description = "IP Address of server built with Server Module"
+  value       = aws_instance.web.public_ip
+}
+
+output "public_dns" {
+  value       = aws_instance.web.public_dns
+}
