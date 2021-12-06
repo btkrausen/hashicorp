8998990a745738021f373b41ddc36168328285f8
Gabe Maentz <gabe@maentz.net>
2021-12-05 23:29:01 -0500

    update prereq code for obj 9

--- terraform/Lab Prerequisites/Terraform Cloud Variables/main.tf
+++ terraform/Lab Prerequisites/Terraform Cloud Variables/main.tf
@@ -161,9 +161,9 @@ resource "aws_instance" "ubuntu_server" {
   }
 
   # Leave the first part of the block unchanged and create our `local-exec` provisioner
-  provisioner "local-exec" {
-    command = "chmod 600 ${local_file.private_key_pem.filename}"
-  }
+  # provisioner "local-exec" {
+  #   command = "chmod 600 ${local_file.private_key_pem.filename}"
+  # }
 
   provisioner "remote-exec" {
     inline = [
@@ -209,10 +209,10 @@ resource "tls_private_key" "generated" {
   algorithm = "RSA"
 }
 
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.generated.private_key_pem
-  filename = "MyAWSKey.pem"
-}
+# resource "local_file" "private_key_pem" {
+#   content  = tls_private_key.generated.private_key_pem
+#   filename = "MyAWSKey.pem"
+# }
 
 resource "aws_key_pair" "generated" {
   key_name   = "MyAWSKey"
@@ -283,9 +283,9 @@ resource "aws_instance" "web_server" {
   }
 
   # Leave the first part of the block unchanged and create our `local-exec` provisioner
-  provisioner "local-exec" {
-    command = "chmod 600 ${local_file.private_key_pem.filename}"
-  }
+  # provisioner "local-exec" {
+  #   command = "chmod 600 ${local_file.private_key_pem.filename}"
+  # }
 
   provisioner "remote-exec" {
     inline = [
