adcef62ab8b17a963cd18fb378abf096fee60845
Bryan Krausen <bkrausen@riverpointtechnology.com>
2021-12-23 14:15:59 -0500

    adding code for prereq

---
+++ terraform/Lab Prerequisites/Terraform Local Variables/variables.tf
@@ -0,0 +1,53 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+variable "environment" {
+  description = "Environment for deployment"
+  type        = string
+  default     = "dev"
+}
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.202.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used for Variables Subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assigment for Variables Subnet"
+  type        = bool
+  default     = true
+}
