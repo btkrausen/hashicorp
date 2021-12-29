53e83d726cef63891d142ad8c38cc36cff7587e5
Gabe Maentz <gabe@maentz.net>
2021-12-29 12:03:38 -0500

    upload prereqs for obj7 and obj9 labs

---
+++ terraform/Lab Prerequisites/Terraform State Backend Configuration/modules/server/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  ami                    = var.ami
+  instance_type          = var.size
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_groups
+
+  tags = {
+    "Name"        = "Server from Module"
+    "Environment" = "Training"
+  }
+}
