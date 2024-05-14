5bc0c262da704f63e8935030f9a9da30eff938c8
Bryan Krausen <bkrausen@riverpointtechnology.com>
2024-05-14 08:37:47 -0400

    removing domain parameter

--- terraform/Lab Prerequisites/Section 06 - Interact with Terraform Modules/Terraform Modules/main.tf
+++ terraform/Lab Prerequisites/Section 06 - Interact with Terraform Modules/Terraform Modules/main.tf
@@ -107,7 +107,6 @@ resource "aws_internet_gateway" "internet_gateway" {
 
 #Create EIP for NAT Gateway
 resource "aws_eip" "nat_gateway_eip" {
-  domain     = "vpc"
   depends_on = [aws_internet_gateway.internet_gateway]
   tags = {
     Name = "demo_igw_eip"
